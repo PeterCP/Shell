181dcb5d4ddeb78a0d4c272acd138cc0191d5d5f
PeterCP <pacp95@gmail.com>
2016-08-10 12:06:57 -0500

    Add color to grep and simplify ls aliases.

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,11 +1,14 @@
+# Alias to add color to 'grep' command.
+alias grep="grep --color=auto"
+
 # Add shorcut aliases for 'ls' command.
 alias ls="ls -GFh"
 alias la="ls -a"
-alias ll="ls -l"
-alias lal="ls -al"
-alias lg="lal | grep"
+alias l="ls -l"
+alias ll="ls -al"
+alias lg="ll | grep"
 lm(){
-    lal "$@" | more
+    ll "$@" | more
 }
 
 # Alias for 'brew services'
